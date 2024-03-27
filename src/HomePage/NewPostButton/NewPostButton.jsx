10dd68fe5685b32ac138ce08f2e2cd1e790a335a
madroakos <madroboy@protonmail.com>
2024-03-27 18:29:09 +0100

    Add functionality for submitting posts

--- src/HomePage/NewPostButton/NewPostButton.jsx
+++ src/HomePage/NewPostButton/NewPostButton.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import './NewPostButton.css';
+import {SUBMIT_POST_URL} from "../../auth/backendConfig";
+
 const NewPostButton = () => {
-    const [isHidden, setIsHidden] = React.useState(true);
+    const [isHidden, setIsHidden] = useState(true);
+    const [newPostComment, setNewPostComment] = useState(''); // Add this line
 
     const handleClick = () => {
         setIsHidden(!isHidden);
+    }
+
+    const handlePostChange = (event) => {
+        setNewPostComment(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        fetch(SUBMIT_POST_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`},
+            body: JSON.stringify({ postContent: newPostComment })
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log('Post saved successfully');
+                } else {
+                    throw new Error('Error saving post');
+                }
+            });
+        setNewPostComment('');
     }
 
     return (
@@ -12,11 +37,17 @@ const NewPostButton = () => {
             <button id="newPostButton" onClick={handleClick}>+</button>
             <div id="newPost" className={`newPostButton ${isHidden ? 'hidden' : ''}`}>
                 <label>
-                    <textarea id="newPostComment" placeholder="Type here..." maxLength="200"></textarea>
+                    <textarea
+                        id="newPostComment"
+                        placeholder="Type here..."
+                        maxLength="200"
+                        value={newPostComment}
+                        onChange={handlePostChange}
+                    ></textarea>
                 </label>
                 <div>
                     <label id="remainingCounter">200</label>
-                    <button id="submitButton">Submit</button>
+                    <button id="submitButton" onClick={handleSubmit}>Submit</button>
                 </div>
             </div>
         </div>
--- src/auth/backendConfig.js
+++ src/auth/backendConfig.js
@@ -4,4 +4,5 @@ const REGISTER_URL = `${BASE_URL}/api/auth/register`;
 const VALIDATE_URL = `${BASE_URL}/api/auth/validateToken`;
 const POSTS_URL = `${BASE_URL}/posts`;
 const POSTS_BY_USER_URL = `${BASE_URL}/postsByUser`;
-export {LOGIN_URL, REGISTER_URL, VALIDATE_URL, POSTS_URL, POSTS_BY_USER_URL};
+const SUBMIT_POST_URL = `${BASE_URL}/submitPost`;
+export {LOGIN_URL, REGISTER_URL, VALIDATE_URL, POSTS_URL, POSTS_BY_USER_URL, SUBMIT_POST_URL};
